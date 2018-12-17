f44115afdeb3c78637739fb40a1aced01b4ea386
manthandubal <manthan.dubal@gmail.com>
2018-12-18 02:14:47 +0530

    Added Date regx for date validation

--- UnstrcturedDataProcessing.java
+++ UnstrcturedDataProcessing.java
@@ -1,10 +1,15 @@
 package HadoopProcess;
+import java.util.StringTokenizer;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UnstructuredDataHandling {
 
@@ -17,13 +22,35 @@ public class UnstructuredDataHandling {
 		try {
 			BufferedReader br = new BufferedReader(new FileReader("E:\\Eclise_Workspace\\JavaProject\\src\\HelperFiles\\localhost.2008-06-29.log.log"));
 			String line;
-			int lineCount=0;
-			 while ((line = br.readLine()) != null) {
-				 lineCount++;
-				 System.out.println(line);
+			int lineCount = 0;
+			String DateString;
+			List<String> Time= new ArrayList<String>();
+			List<String> Error= new ArrayList<String>();
+			int ErrorCount = 0;
+			String ErrorString;
+			while ((line = br.readLine()) != null) {
+				lineCount++;
+				try {
+					DateString=line.substring(0, 24);
+					
+					if (isValidDate(DateString))
+					 {
+						 Time.add(ErrorCount, DateString);
+						 ErrorString = line.substring(24);
+						 Error.add(ErrorCount, ErrorString.trim()); 
+					 }
+				}
+				catch(StringIndexOutOfBoundsException ae)
+				{
+					continue;
+				}
 			 }
-			 System.out.println("Read lines"+lineCount);
-			 br.close();
+			for(int i=0; i<Time.size();i++) {
+				System.out.println(Time.get(i));
+				System.out.println(Error.get(i));;
+			}
+			System.out.println(Time.size() +" " + Error.size() );
+			br.close();
 		} catch (FileNotFoundException e1) {
 			// TODO Auto-generated catch block
 			System.out.println("File Not Found");
@@ -33,5 +60,22 @@ public class UnstructuredDataHandling {
 		}
 		System.out.println("File has been closed..!!");
 	}
-
+	private static boolean isValidDate(String inputString)
+	{
+		if(inputString.trim().matches("^([A-Z][a-z][a-z])( )([0-3][0-9]), \\d{4} (00|[0-9]|1[0-9]|2[0-3]):([0-9]|[0-5][0-9]):([0-9]|[0-5][0-9]) (AM|PM)"))
+			return true;
+		else
+			return false;
+		/*SimpleDateFormat format;
+		format = new SimpleDateFormat("mmm dd, yyyy HH:MM:SS");
+		try
+		{
+			format.parse(inputString);
+			return true;
+		}
+		catch(ParseException e)
+		{
+			return isValidDate2(inputString);
+		}*/
+	}
 }
